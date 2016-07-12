a086e6ae8a0af7b23bec39f63aa940c67aec2b42
Sergey Grebenshchikov <sgreben@gmail.com>
2016-07-12 13:08:38 +0200

    Cosmetics

--- src/main/java/sgreben/regex-builder/tokens/TOKEN.java
+++ src/main/java/sgreben/regex-builder/tokens/TOKEN.java
@@ -1,8 +1,5 @@
 package sgreben.regex_builder.tokens;
 
-// Right now, the "tokens" is just regex tokens. 
-// If we switch to a custom regex engine, this will contain the regex VM instructions.
-
 public interface TOKEN {
 	String regexString();
 }
