00a26ac884e7c0c2dd0921a74660cd195ed3a216
Sergey Grebenshchikov <sgreben@gmail.com>
2016-07-23 17:08:47 +0200

    Remove unused field

--- src/main/java/sgreben/regex-builder/CaptureGroup.java
+++ src/main/java/sgreben/regex-builder/CaptureGroup.java
@@ -1,12 +1,10 @@
 package sgreben.regex_builder;
 
-import java.util.List;
 import sgreben.regex_builder.expression.*;
 import sgreben.regex_builder.tokens.*;
 
 public class CaptureGroup extends Unary implements Expression {
-	private java.util.regex.Matcher rawMatcher;
-	
+
 	public CaptureGroup(Expression expression) { super(expression); }
 	
 	public void compile(java.util.List<TOKEN> output) {
