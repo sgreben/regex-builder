d56f205169043c05d735e8ff6a29614ce49eabc2
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2016-10-04 19:50:36 +0200

    Added Posix and Java character classes

---
+++ src/main/java/sgreben/regex_builder/tokens/CHAR_CLASS_NAMED.java
@@ -0,0 +1,16 @@
+package sgreben.regex_builder.tokens;
+
+public class CHAR_CLASS_NAMED implements TOKEN {
+    private final String name;
+    private final String regexString;
+
+    public CHAR_CLASS_NAMED(String name) {
+        this.name = name;
+        this.regexString = "\\p{"+name+"}";
+    }
+
+    @Override
+    public String regexString() {
+        return regexString;
+    }
+}
