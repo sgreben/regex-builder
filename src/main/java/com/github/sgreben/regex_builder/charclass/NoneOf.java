73e674bcb3aa583bda55f2eec56744b4f5920456
Stefan Lobbenmeier <Stefan.Lobbenmeier@gmail.com>
2019-02-28 10:01:07 +0100

    Added NoneOf CharClass

---
+++ src/main/java/com/github/sgreben/regex_builder/charclass/NoneOf.java
@@ -0,0 +1,20 @@
+package com.github.sgreben.regex_builder.charclass;
+
+import com.github.sgreben.regex_builder.tokens.CARET;
+import com.github.sgreben.regex_builder.tokens.END_CHAR_CLASS;
+import com.github.sgreben.regex_builder.tokens.RAW;
+import com.github.sgreben.regex_builder.tokens.START_CHAR_CLASS;
+import com.github.sgreben.regex_builder.tokens.TOKEN;
+
+public class NoneOf extends Nullary {
+	private final String chars;
+	public NoneOf(String chars) {
+		this.chars = chars;
+	}
+	public void compile(java.util.List<TOKEN> output) {
+		output.add(new START_CHAR_CLASS());
+		output.add(new CARET());
+		output.add(new RAW(chars));
+		output.add(new END_CHAR_CLASS());
+	}
+}
