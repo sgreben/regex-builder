fa488f9db95e505be5d64f6e660ea3b5c0eb835e
Stefan Lobbenmeier <Stefan.Lobbenmeier@gmail.com>
2019-02-28 10:29:56 +0100

    Added Tests for HexDigit and (ignored) nonHexDigit

--- src/test/java/com/github/sgreben/regex_builder/CharClassTest.java
+++ src/test/java/com/github/sgreben/regex_builder/CharClassTest.java
@@ -2,6 +2,7 @@ package com.github.sgreben.regex_builder;
 
 import static org.junit.Assert.assertThat;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static com.github.sgreben.regex_builder.hamcrest.MatchesPattern.*;
@@ -11,6 +12,29 @@ import static com.github.sgreben.regex_builder.CharClass.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class CharClassTest {
+
+	@Test
+	public void testHexDigit() throws Exception {
+		assertCharClassMatch(hexDigit(), "0123456789");
+		assertCharClassMatch(hexDigit(), "abcdef");
+		assertCharClassMatch(hexDigit(), "ABCDEF");
+
+		assertCharClassMismatch(hexDigit(), "G");
+		assertCharClassMismatch(hexDigit(), "[");
+		assertCharClassMismatch(hexDigit(), "]");
+	}
+
+	@Ignore
+	@Test
+	public void testNonHexDigit() throws Exception {
+		assertCharClassMismatch(nonHexDigit(), "0123456789");
+		assertCharClassMismatch(nonHexDigit(), "abcdef");
+		assertCharClassMatch(nonHexDigit(), "ABCDEF");
+
+		assertCharClassMatch(nonHexDigit(), "G");
+		assertCharClassMatch(nonHexDigit(), "[");
+		assertCharClassMatch(nonHexDigit(), "]");
+	}
 
 	@Test
 	public void testOneOf() throws Exception {
