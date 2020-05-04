5e02bb3f19a64205f63cae66a3406aa69e9b9fa4
Sergey Grebenshchikov <sgreben@users.noreply.github.com>
2020-05-04 22:04:58 +0200

    Support pattern flags

--- src/main/java/com/github/sgreben/regex_builder/Pattern.java
+++ src/main/java/com/github/sgreben/regex_builder/Pattern.java
@@ -5,46 +5,49 @@ import com.github.sgreben.regex_builder.compiler.Compiler;
 public class Pattern {
 	private final java.util.regex.Pattern rawPattern;
 	private final CaptureGroupIndex groupIndex;
-	
+
 	public static Pattern compile(Expression expression) {
 		return Compiler.compile(expression);
 	}
-	
+
+	public static Pattern compile(Expression expression, int flags) {
+		return Compiler.compile(expression, flags);
+	}
+
 	public static Pattern quote(String literal) {
 		return Compiler.compile(Re.string(literal));
 	}
-	
-	public Pattern(java.util.regex.Pattern rawPattern, 
-				   CaptureGroupIndex groupIndex) {
+
+	public Pattern(java.util.regex.Pattern rawPattern, CaptureGroupIndex groupIndex) {
 		this.rawPattern = rawPattern;
 		this.groupIndex = groupIndex;
 	}
-	
+
 	public Matcher matcher(CharSequence input) {
 		java.util.regex.Matcher matcher = rawPattern.matcher(input);
 		return new Matcher(matcher, groupIndex);
 	}
-	
+
 	public static boolean matches(Expression regex, CharSequence input) {
 		return compile(regex).matcher(input).matches();
 	}
-	
+
 	public String[] split(CharSequence input) {
 		return rawPattern.split(input);
 	}
-	
+
 	public String[] split(CharSequence input, int limit) {
 		return rawPattern.split(input, limit);
 	}
-	
+
 	public java.util.stream.Stream<String> splitAsStream(CharSequence input) {
 		return rawPattern.splitAsStream(input);
 	}
-	
+
 	public String pattern() {
 		return rawPattern.pattern();
 	}
-	
+
 	@Override
 	public String toString() {
 		return rawPattern.toString();
--- src/main/java/com/github/sgreben/regex_builder/compiler/Compiler.java
+++ src/main/java/com/github/sgreben/regex_builder/compiler/Compiler.java
@@ -1,15 +1,18 @@
 package com.github.sgreben.regex_builder.compiler;
 
 import java.util.LinkedList;
-
 import com.github.sgreben.regex_builder.CaptureGroup;
 import com.github.sgreben.regex_builder.CaptureGroupIndex;
-import com.github.sgreben.regex_builder.Pattern;
 import com.github.sgreben.regex_builder.Expression;
+import com.github.sgreben.regex_builder.Pattern;
 import com.github.sgreben.regex_builder.tokens.TOKEN;
 
 public class Compiler {
 	public static Pattern compile(Expression expression) {
+		return compile(expression, 0);
+	}
+
+	public static Pattern compile(Expression expression, final int flags) {
 		CaptureGroupVisitor visitor = new CaptureGroupVisitor();
 		CaptureGroup entireMatch = new CaptureGroup(expression);
 		LinkedList<TOKEN> tokens = new LinkedList<TOKEN>();
@@ -17,12 +20,12 @@ public class Compiler {
 		entireMatch.accept(visitor);
 		CaptureGroupIndex index = visitor.get();
 		entireMatch.compile(index, tokens);
-		for(TOKEN op : tokens) {
+		for (TOKEN op : tokens) {
 			sb.append(op.regexString());
 		}
 		String regexString = sb.toString();
-		java.util.regex.Pattern rawPattern = java.util.regex.Pattern.compile(regexString);
+		java.util.regex.Pattern rawPattern = java.util.regex.Pattern.compile(regexString, flags);
 		return new Pattern(rawPattern, index);
 	}
-	
+
 }
